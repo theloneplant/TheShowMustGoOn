994e96b8b7fcc619b97f3f3866433b3a043be90c
Michael LaPlante <mflaplan@ucsd.edu>
2015-01-24 19:49:18 -0800

    Microphone script programmed

--- TheShowMustGoOn/Assets/Scripts/BeginSinging.cs
+++ TheShowMustGoOn/Assets/Scripts/BeginSinging.cs
@@ -4,8 +4,14 @@ using System.Collections;
 public class BeginSinging : MonoBehaviour
 {
 	public GameObject player;
+	public GameObject[] rafters;
+	public GameObject sparkEmitter;
+	public GameObject[] lights;
+	public GameObject deadLight;
+	public float flickerTime, waitForRafters;
 
-	private bool colliding, singing;
+	private float startTime;
+	private bool colliding, singing, flickering, raftersFell;
 	private Vector3 anchor;
 
 	void Start ()
@@ -20,16 +26,68 @@ public class BeginSinging : MonoBehaviour
 			if (Input.GetKeyDown(KeyCode.F))
 			{
 				singing = true;
+				startTime = Time.time;
 				anchor = player.transform.position;
 			}
 		}
 
 		if (singing)
 		{
-			Debug.Log("IM SINGINGGG");
+			// Hold player
 			player.transform.position = anchor;
 
-			// Start scripted event
+			if (Time.time - startTime >= flickerTime)
+			{
+				startTime = Time.time;
+				flickering = true;
+				singing = false;
+
+				deadLight.light.enabled = false;
+				sparkEmitter.particleEmitter.Emit();
+			}
+		}
+
+		if (flickering)
+		{
+			// Hold player
+			player.transform.position = anchor;
+
+			// Flicker lights
+			for (int i = 0; i < lights.Length; i++)
+			{
+				if (Random.Range(0, 100) >= 50)
+				{
+					lights[i].light.enabled = false;
+				}
+				else
+				{
+					lights[i].light.enabled = true;
+				}
+			}
+			
+			if (Time.time - startTime >= waitForRafters)
+			{
+				startTime = Time.time;
+				raftersFell = true;
+				flickering = false;
+
+				// Enable all lights
+				for (int i = 0; i < lights.Length; i++)
+				{
+					lights[i].light.enabled = true;
+				}
+
+				for (int i = 0; i < rafters.Length; i++)
+				{
+					rafters[i].rigidbody.isKinematic = false;
+					rafters[i].rigidbody.useGravity = true;
+				}
+			}
+		}
+
+		if (raftersFell)
+		{
+
 		}
 	}
 
