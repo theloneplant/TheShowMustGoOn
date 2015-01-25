fec5c10441645dc45d0a2806b0f1dc059496ceb0
Michael LaPlante <mflaplan@ucsd.edu>
2015-01-25 05:27:18 -0800

    Merging

---
+++ TheShowMustGoOn/Assets/ThrowTrash.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowTrash : MonoBehaviour
+{
+	public AudienceHappiness happiness;
+	public GameObject[] trash;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+}
